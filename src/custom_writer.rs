b9dd7c17a2dbb0e10fe5a126e4a5e136126df8ca
Edenys Deniz González <holofist1@gmail.com>
2023-07-12 14:34:59 -0400

    feat: add writer

---
+++ src/custom_writer.rs
@@ -0,0 +1,69 @@
+use std::{fs::File, io::BufWriter};
+
+use ustr::UstrMap;
+
+pub fn write_exclusivity_per_pub(
+    exclusivity_per_pub: UstrMap<UstrMap<Vec<(usize, f64)>>>,
+    path_dir: &String,
+) {
+    println!("Init writer for exclusivity_per_pub");
+    serde_json::to_writer_pretty(
+        BufWriter::with_capacity(
+            1000000,
+            File::create(path_dir.clone() + "exclusivity_per_pub.json")
+                .expect("Failed to create file."),
+        ),
+        &exclusivity_per_pub,
+    )
+    .expect("Failed to write file");
+
+    println!("Finish writer for exclusivity_per_pub");
+}
+
+pub fn write_co_authorship_freq(co_authorship_freq: UstrMap<UstrMap<f64>>, path_dir: &String) {
+    println!("Init writer for co_authorship_freq");
+    serde_json::to_writer(
+        BufWriter::with_capacity(
+            1000000,
+            File::create(path_dir.clone() + "co_authorship_freq.json")
+                .expect("Failed to create file."),
+        ),
+        &co_authorship_freq,
+    )
+    .expect("Failed to write file");
+
+    println!("Finish writer for co_authorship_freq");
+}
+
+pub fn write_total_co_authorship_freq_per_author(
+    total_co_authorship_freq_per_author: UstrMap<f64>,
+    path_dir: &String,
+) {
+    println!("Init writer for total_co_authorship_freq_per_author");
+    serde_json::to_writer(
+        BufWriter::with_capacity(
+            1000000,
+            File::create(path_dir.clone() + "total_co_authorship_freq_per_author.json")
+                .expect("Failed to create file."),
+        ),
+        &total_co_authorship_freq_per_author,
+    )
+    .expect("Failed to write file");
+
+    println!("Finish writer for total_co_authorship_freq_per_author");
+}
+
+pub fn write_normalized_weights(normalized_weights: UstrMap<UstrMap<f64>>, path_dir: &String) {
+    println!("Init writer for normalized_weights");
+    serde_json::to_writer(
+        BufWriter::with_capacity(
+            1000000,
+            File::create(path_dir.clone() + "normalized_weights.json")
+                .expect("Failed to create file."),
+        ),
+        &normalized_weights,
+    )
+    .expect("Failed to write file");
+
+    println!("Finish writer for normalized_weights");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,30 @@
+mod custom_writer;
 mod reader;
 mod types;
 mod utils;
 
-use crate::reader::read_publication_list;
-use std::{
-    fs::{create_dir, File},
-    io::BufWriter,
-    path::Path,
-    process,
+use crate::{
+    custom_writer::{
+        write_co_authorship_freq, write_exclusivity_per_pub, write_normalized_weights,
+        write_total_co_authorship_freq_per_author,
+    },
+    reader::read_publication_list,
 };
+use std::{fs::create_dir, path::Path, process};
 
 fn execute_calculations(pub_list: Vec<types::SimplePublication>, path_dir: String) {
     println!("calculating exclusivity_per_pub");
     let exclusivity_per_pub = utils::calculations::calculate_exclusivity_per_pub(&pub_list);
-    serde_json::to_writer(
-        BufWriter::with_capacity(
-            1000000,
-            File::create(path_dir.clone() + "exclusivity_per_pub.json")
-                .expect("Failed to create file."),
-        ),
-        &exclusivity_per_pub,
-    )
-    .expect("Failed to write file");
     println!("exclusivity_per_pub finished");
 
     println!("calculating co_authorship_freq");
     let co_authorship_freq =
         utils::calculations::calculate_co_authorship_freq(&exclusivity_per_pub);
-    serde_json::to_writer(
-        BufWriter::with_capacity(
-            1000000,
-            File::create(path_dir.clone() + "co_authorship_freq.json")
-                .expect("Failed to create file."),
-        ),
-        &co_authorship_freq,
-    )
-    .expect("Failed to write file");
     println!("co_authorship_freq finished");
 
     println!("calculating total_co_authorship_freq_per_author");
     let total_co_authorship_freq_per_author =
         utils::calculations::calculate_total_co_authorship_freq_per_author(&co_authorship_freq);
-    serde_json::to_writer(
-        BufWriter::with_capacity(
-            1000000,
-            File::create(path_dir.clone() + "total_co_authorship_freq_per_author.json")
-                .expect("Failed to create file."),
-        ),
-        &total_co_authorship_freq_per_author,
-    )
-    .expect("Failed to write file");
     println!("total_co_authorship_freq_per_author finished");
 
     println!("calculating normalized_weights");
@@ -57,16 +32,12 @@ fn execute_calculations(pub_list: Vec<types::SimplePublication>, path_dir: Strin
         &co_authorship_freq,
         &total_co_authorship_freq_per_author,
     );
-    serde_json::to_writer(
-        BufWriter::with_capacity(
-            1000000,
-            File::create(path_dir.clone() + "normalized_weights.json")
-                .expect("Failed to create file."),
-        ),
-        &normalized_weights,
-    )
-    .expect("Failed to write file");
     println!("normalized_weights finished");
+
+    write_exclusivity_per_pub(exclusivity_per_pub, &path_dir);
+    write_co_authorship_freq(co_authorship_freq, &path_dir);
+    write_total_co_authorship_freq_per_author(total_co_authorship_freq_per_author, &path_dir);
+    write_normalized_weights(normalized_weights, &path_dir);
 }
 
 fn main() {
